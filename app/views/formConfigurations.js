dc691895227b5eae7a8662b000532f43e1092ed3
José Ángel <201400189@estudiantes.upqroo.edu.mx>
2019-09-10 17:33:01 -0500

    Se agrega cambios para inicializar el formulario.

--- app/views/formConfigurations.js
+++ app/views/formConfigurations.js
@@ -1,98 +1,86 @@
 import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
-import {
-    Text,
-    View,
-    TextInput,
-    TouchableHighlight,
-    Image,
-    Alert,
-    Keyboard,
-    ToastAndroid,
-    ActivityIndicator,
-    BackHandler,
-    Button,
-} from 'react-native';
-
-import myStyles from './../styles';
+import t from 'tcomb-form-native'; // 0.6.9
 
 import Entities from 'html-entities';
 
-import AsyncStorage from '@react-native-community/async-storage';
-
 const entities = new Entities.AllHtmlEntities();
 
-export default class LoginView extends Component {
+const Form = t.form.Form;
 
-    constructor(props) {
+const options = {
+    fields: {
+        titulo: {
+            label: entities.decode('T&iacute;tulo')
+        },
+        descripcion: {
+            label: entities.decode('Descripci&oacute;n'),
+        },
+    },
+    i18n: {
+        optional: ' (opcional)',
+        required: '',
+        add: 'Agregar',   // add button
+        remove: '?',  // remove button
+        up: '?',      // move up button
+        down: '?'     // move down button
+      }
+};
 
-        super(props);
+const Person = t.struct({
+    titulo: t.String,              // a required string
+    descripcion: t.maybe(t.String),  // an optional string
+    fase: t.Number,               // a required number
+    prueba: t.Boolean        // a boolean
+});
 
-        this.state = {
-            token: '',
-        };
-        this.loadToken();
-        this.backToMenu = this.backToMenu.bind(this);
+export default class FormConfigurations extends Component {
+    onPress() {
+        // call getValue() to get the values of the form
+        var value = this.refs.form.getValue();
+        if (value) { // if validation fails, value will be null
+            console.log(value); // value here is an instance of Person
+        }
     }
-
-    async loadToken () {
-
-        if (this.props.navigation.getParam('data')) {
-
-            let token = this.props.navigation.getParam('data');
-      
-            this.setState({ token: token });
-      
-          }
-
-    }
-
-    componentDidMount() {
-        BackHandler.addEventListener('hardwareBackPress', this.backToMenu);
-    }
-
-    componentWillUnmount() {
-        BackHandler.removeEventListener('hardwareBackPress', this.backToMenu)
-    }
-
-    backToMenu() {
-        Alert.alert(
-            'Nuevo formulario',
-            entities.decode('Desea descartar los cambios? El formulario no se guardar&aacute;'),
-            [
-                {
-                    text: 'Continuar',
-                    style: 'cancel',
-                },
-                {
-                    text: 'Descartar',
-                    onPress: () => {
-                        this.props.navigation.navigate({
-                            routeName: 'Wait',
-                            params: {
-                                goTo: null,
-                            }})
-                        return true;
-                    }
-                },
-            ],
-            { cancelable: true },
-        );
-        return true;
-
-    }
-
-    
 
     render() {
         return (
-            <View style={myStyles.container}>
-
-                <Text>{entities.decode('Entr&oacute; a configuraci&oacute;n')}</Text>
-
+            <View style={styles.container}>
+                {/* display */}
+                <Form
+                    ref="form"
+                    type={Person}
+                    options={options}
+                />
+                <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
+                    <Text style={styles.buttonText}>Guardar</Text>
+                </TouchableHighlight>
             </View>
         );
     }
 }
 
-
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        marginTop: 50,
+        padding: 20,
+        backgroundColor: '#ffffff',
+    },
+    buttonText: {
+        fontSize: 18,
+        color: 'white',
+        alignSelf: 'center'
+    },
+    button: {
+        height: 36,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        borderWidth: 1,
+        borderRadius: 8,
+        marginBottom: 10,
+        alignSelf: 'stretch',
+        justifyContent: 'center'
+    }
+});
